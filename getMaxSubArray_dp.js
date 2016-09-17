d5e2920a980b0a2338e4491050148e18837fb8d3
0704681032 <307517019@qq.com>
2016-09-17 22:21:37 +0800

    Update getMaxSubArray_dp.js

--- getMaxSubArray_dp.js
+++ getMaxSubArray_dp.js
@@ -1,13 +1,26 @@
-<script type="text/javascript">
-var _py = _py || [];
-_py.push(['a', 'MC..o8vMMWxEXDCiqYckD81lUX']);
-_py.push(['domain','stats.ipinyou.com']);
-_py.push(['e','']);
--function(d) {
-var s = d.createElement('script'),
-e = d.body.getElementsByTagName('script')[0]; e.parentNode.insertBefore(s, e),
-f = 'https:' == location.protocol;
-s.src = (f ? 'https' : 'http') + '://'+(f?'fm.ipinyou.com':'fm.p0y.cn')+'/j/adv.js';
-}(document);
-</script>
-<noscript><img src="//stats.ipinyou.com/adv.gif?a=MC..o8vMMWxEXDCiqYckD81lUX&e=" style="display:none;"/></noscript>
+function getMaxSubArr( arr ) {
+	let max = arr[0];
+	let dp = [];
+	dp[0] = max ;
+	let result = [];
+	result[0] = max;
+
+	let len = arr.length;
+	for( let i = 1 ; i < len ; i++ ){
+		let tmp = dp[i-1] + arr[i];
+		if ( tmp >= arr[i] ) { 
+			dp[i] = tmp;
+		} else {
+			result = [];//reset
+			dp[i] = arr[i];
+		}
+		result.push(i);
+		max = Math.max.call(null,max,dp[i]);
+
+	}
+	console.log(dp);
+	console.log(result);
+	return max;
+}
+
+console.log(getMaxSubArr([2,-3,4,5,-4,-6,7,-8]));
